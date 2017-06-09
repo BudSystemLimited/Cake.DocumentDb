aacfc85ce0a9f2677346f966ad1e18078f22e2fd
Jonathan Stowell <jonathan.stowell@chorus.co>
2017-06-09 14:11:44 +0100

    Embedded Document Seed Base Class.

---
+++ Cake.DocumentDb/EmbeddedDocumentSeed.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Cake.DocumentDb.Interfaces;
+using Newtonsoft.Json;
+
+namespace Cake.DocumentDb
+{
+    public abstract class EmbeddedDocumentSeed : ISeedDocument
+    {
+        public abstract string FriendlyName { get; }
+        public abstract string Database { get; }
+        public abstract string Collection { get; }
+        public abstract string PartitionKey { get; }
+        public abstract string DocumentName { get; }
+        public object Document()
+        {
+            using (var stream = Assembly.GetAssembly(GetType()).GetManifestResourceStream(
+                GetType(),
+                DocumentName))
+            {
+                if (stream == null)
+                    throw new NotSupportedException($"Could not load document");
+
+                using (var reader = new StreamReader(stream))
+                {
+                    return JsonConvert.DeserializeObject(reader.ReadToEnd());
+                }
+            }
+        }
+    }
+}
